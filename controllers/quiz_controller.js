fb3c45696f0a126a2fe2200b1eed3ef623cacdce
lsblanco <lsblanco@gmail.com>
2015-07-13 13:53:47 +0200

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,10 +1,10 @@
 //Get /quizes/question
-exports.question = funtion(req, res){
+exports.question = function(req, res){
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 //Get /quizes/answer
-exports.answer = funtion(req, res){
+exports.answer = function(req, res){
 	if(req.query.respuesta === 'Roma'){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	} else{
