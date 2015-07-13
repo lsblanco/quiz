e2eda270b2043adb1c1c77807924cb684f1991eb
lsblanco <lsblanco@gmail.com>
2015-07-13 20:40:40 +0200

    “creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/autor', function(req, res) {
+  res.render('autor', { nombre: 'Santiago', video:'Tachan!!!!', foto:':-)' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
